33070b12a1b00e754ffad097dc2d60ba1ac68fe5
Daniel Kohler <11864045+ddkohler@users.noreply.github.com>
2026-06-28 10:33:39 -0500

    photon counting (#46)
    * Update _pygage.py
    
    * close methods
    
    * refactor segment_count_limits
    
    * Update _compuscope.py
    
    replace print with log info
    
    * pygage comm wrappers
    
    wrap functions with a treatment that frees handles.
    
    * apply wrappers
    
    remove close method (unnecessary)
    factor out code blocks that invoke pygage
    
    * Update gage-chopping.toml
    
    * setters return none
    
    * Update _chopping.py
    
    implement quick limits check for edge_width_count
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _compuscope.py
    
    commit params at time of measurement
    
    * judicial use of free_system
    
    I lose the handle if I free it, so I cannot do it without completely re-initializing
    
    * first working example
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add debug info
    
    plan to remove later
    
    * Update .gitignore
    
    * gage-chopping works
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * trigger level is now an int
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * photon counting: attempt 1
    
    * Update _chopping.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update _chopping.py
    
    use mean for diff
    
    * Update _chopping.py
    
    * Update gage-chopping.toml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add channel names and units
    
    * bugfixes
    
    * threshold params
    
    remove typos, remove photon_index
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * typos
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestion from @ddkohler
    
    * Apply suggestion from @ddkohler
    
    * cleanup
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- yaqd_gage/_chopping.py
+++ yaqd_gage/_chopping.py
@@ -10,7 +10,7 @@ import numpy as np  # type: ignore
 from yaqd_core import HasMeasureTrigger, IsSensor, IsDaemon
 
 from ._constants import acq_status_codes, transfer_modes
-from ._pygage import PyGage, to_voltage
+from ._pygage import PyGage, uses_pygage, async_uses_pygage, to_voltage
 
 
 impedences = {"fifty": 50, "onemeg": 1_000_000}
@@ -22,6 +22,31 @@ class CompuScope(HasMeasureTrigger, IsSensor, IsDaemon):
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
         self._pg = PyGage()
+        self._channel_names = []
+        self._max_segment_count = None  # redefined in _config_pygage
+        self._config_pygage()
+        self._channel_names.append("ai1")
+        self._channel_names.append("ai2")
+        self._channel_names.append("ai3")
+        for pre in "ap":
+            self._channel_names += [
+                f"{pre}i0",
+                f"{pre}i0_a",
+                f"{pre}i0_b",
+                f"{pre}i0_c",
+                f"{pre}i0_d",
+                f"{pre}i0_diff_abcd",
+                f"{pre}i0_diff_ab",
+                f"{pre}i0_diff_ad",
+            ]
+        self._channel_units = {k: "V" if k.startswith("a") else None for k in self._channel_names}
+        self._samples: Dict[str, np.ndarray] = dict()
+        self._segments: Dict[str, np.ndarray] = dict()
+
+    @uses_pygage
+    def _config_pygage(self):
+        acq = self._pg.get_acquisition_config()
+        self.logger.info(acq)
         # acqusition config
         config = {}
         config["Mode"] = self._config["mode"]
@@ -30,55 +55,48 @@ class CompuScope(HasMeasureTrigger, IsSensor, IsDaemon):
         config["SegmentSize"] = self._config["segment_size"]
         config["TriggerDelay"] = self._config["trigger_delay"]
         config["SegmentCount"] = self._state["segment_count"]
-        config["TriggerTimeOut"] = self._config["trigger_time_out"]
-        config["TriggerHoldOff"] = self._config["trigger_hold_off"]
+        config["TriggerTimeout"] = self._config["trigger_time_out"]
+        config["TriggerHoldoff"] = self._config["trigger_hold_off"]
         config["ExtClk"] = int(self._config["ext_clk"])
-        config["TimeStampMode"] = self._config["time_stamp_mode"]
-        config["TimeStampClock"] = self._config["time_stamp_clock"]
+        config["TimeStampConfig"] = 0
+        # from state
+        for k in config:
+            self.logger.info(f"{k}: {acq.get(k)} | {config.get(k)}")
+        config["SegmentCount"] = self._state["segment_count"]
         self._pg.set_acquisition_config(config)
         self._pg.set_multiple_rec_average_count(self._state["record_count"])
         # channel config
         for channel_index, channel in enumerate(self._config["channels"]):
+            self.logger.info(f"{channel_index=}")
+            # cfg = self._pg.get_channel_config(channel_index)
+            # self.logger.info(cfg)
             config = {}
             config["InputRange"] = channel["range"]
             couplings = {"DC": 1, "AC": 2}
             config["Coupling"] = couplings[channel["coupling"]]
             config["Impedance"] = impedences[channel["impedance"]]
-            config["DiffInput"] = int(channel["diff_input"])
-            config["DirectADC"] = int(channel["direct_adc"])
             config["Filter"] = int(channel["filter"])
             config["DcOffset"] = channel["dc_offset"]
             self._pg.set_channel_config(channel_index + 1, config)
         # trigger config
         for trigger_index, trigger in enumerate(self._config["triggers"]):
+            tcfg = self._pg.get_trigger_config(trigger_index + 1)
+            self.logger.info(tcfg)
             config = {}
             config["Condition"] = trigger["condition"]
-            config["Level"] = trigger["level"]
+            config["Level"] = int(trigger["level"])
             config["Source"] = trigger["source"]
-            config["InputRange"] = trigger["range"]
-            config["Impedance"] = impedences[channel["impedance"]]
+            config["ExtRange"] = trigger["range"]
+            config["ExtImpedance"] = impedences[channel["impedance"]]
+            config["ExtCoupling"] = couplings[channel["coupling"]]
             config["Relation"] = 0
+            for k in tcfg:
+                self.logger.info(f"{k}: {tcfg.get(k)} | {config.get(k)}")
+
             self._pg.set_trigger_config(trigger_index + 1, config)
         # finish
         self._pg.commit()
-        self._channel_names = []
-        self._channel_names.append("ai0")
-        self._channel_names.append("ai1")
-        self._channel_names.append("ai2")
-        self._channel_names.append("ai3")
-        self._channel_names.append("ai0_a")
-        self._channel_names.append("ai0_b")
-        self._channel_names.append("ai0_c")
-        self._channel_names.append("ai0_d")
-        self._channel_names.append("ai0_diff_abcd")
-        self._channel_names.append("ai0_diff_ab")
-        self._channel_names.append("ai0_diff_ad")
-        self._channel_units = {k: "V" for k in self._channel_names}
-        self._samples: Dict[str, np.ndarray] = dict()
-        self._segments: Dict[str, np.ndarray] = dict()
-        self._segment_count_limits = [1, self._pg.max_segment_count]
-        assert self._state["segment_count"] <= self._segment_count_limits[1]
-        self.set_segment_count(self._state["segment_count"])
+        self._max_segment_count = self._pg.max_segment_count
 
     def get_edge_width_count(self) -> int:
         return self._state["edge_width_count"]
@@ -96,30 +114,31 @@ class CompuScope(HasMeasureTrigger, IsSensor, IsDaemon):
         return self._state["segment_count"]
 
     def get_segment_count_limits(self) -> List[int]:
-        return self._segment_count_limits
+        return [1, self._max_segment_count]
 
+    @async_uses_pygage
     async def _measure(self):
-        self._segment_count_limits = [1, self._pg.max_segment_count]
         out = dict()
-        assert self._state["segment_count"] <= self._segment_count_limits[1]
-        assert self._state["edge_width_count"] >= 0  # sanity
-        # set segment_count, record_count
-        segment_count = self._state["segment_count"]
-        record_count = self._state["record_count"]
-        self._pg.set_acquisition_config({"SegmentCount": segment_count})
-        self._pg.set_multiple_rec_average_count(record_count)
+        # apply state variables
+        photon_threshold = self._state["photon_threshold"]
+        self._pg.set_acquisition_config({"SegmentCount": self._state["segment_count"]})
+        self._pg.set_multiple_rec_average_count(self._state["record_count"])
         self._pg.commit()
+        self._max_segment_count = self._pg.max_segment_count
         # start capture
+        before = time.time()
         self._pg.start_capture()
         # wait for capture to complete
-        before = time.time()
         while True:
             code = self._pg.get_status()
             if acq_status_codes[code] == "ACQ_STATUS_READY":
                 break
             await asyncio.sleep(0)
         # read out
+        after = time.time()
         segments = {}
+        segment_count = self._state["segment_count"]
+        record_count = self._state["record_count"]
         for i in range(0, len(self._config["channels"])):
             s = await self._process_single_channel(i, segment_count, record_count)
             segments.update(s)
@@ -153,30 +172,36 @@ class CompuScope(HasMeasureTrigger, IsSensor, IsDaemon):
                         self._segments["regions"][sl] = k
                         start = None
             await asyncio.sleep(0)
+        # segments: dict with keys of channel, values are 1D array of 1D arrays
+        # count photon events
+        counts = np.array([shot > photon_threshold for shot in segments["ai0"]], dtype=bool)
+        out["pi0"] = counts.sum()
         # take means
         out["ai0"] = np.mean(segments["ai0"])
         out["ai1"] = np.mean(segments["ai1"])
         out["ai2"] = np.mean(segments["ai2"])
         out["ai3"] = np.mean(segments["ai3"])
-        if regions["a"]:
-            out["ai0_a"] = np.mean(segments["ai0"][np.r_[tuple(regions["a"])]])
-        else:
-            out["ai0_a"] = np.nan
-        if regions["b"]:
-            out["ai0_b"] = np.mean(segments["ai0"][np.r_[tuple(regions["b"])]])
-        else:
-            out["ai0_b"] = np.nan
-        if regions["c"]:
-            out["ai0_c"] = np.mean(segments["ai0"][np.r_[tuple(regions["c"])]])
-        else:
-            out["ai0_c"] = np.nan
-        if regions["d"]:
-            out["ai0_d"] = np.mean(segments["ai0"][np.r_[tuple(regions["d"])]])
-        else:
-            out["ai0_d"] = np.nan
-        out["ai0_diff_abcd"] = out["ai0_a"] - out["ai0_b"] + out["ai0_c"] - out["ai0_d"]
-        out["ai0_diff_ab"] = out["ai0_b"] - out["ai0_a"]
-        out["ai0_diff_ad"] = out["ai0_d"] - out["ai0_a"]
+
+        # chopping-derived channels
+        # TODO: remove hard-code ai0, put in config
+        for phase in "abcd":
+            if regions[phase]:
+                valid = np.r_[tuple(regions[phase])]
+                out[f"pi0_{phase}"] = np.mean(counts[valid])
+                out[f"ai0_{phase}"] = np.mean(segments["ai0"][valid])
+            else:
+                out[f"pi0_{phase}"] = out[f"ai0_{phase}"] = np.nan
+
+        for key in ["pi0", "ai0"]:
+            out[f"{key}_diff_abcd"] = (
+                out[f"{key}_a"] - out[f"{key}_b"] + out[f"{key}_c"] - out[f"{key}_d"]
+            )
+            out[f"{key}_diff_ab"] = out[f"{key}_b"] - out[f"{key}_a"]
+            out[f"{key}_diff_ad"] = out[f"{key}_d"] - out[f"{key}_a"]
+        finished = time.time()
+        self.logger.info(f"measurement: {after-before} sec")
+        self.logger.info(f"maths: {finished-after} sec")
+
         return out
 
     async def _process_single_channel(
@@ -228,13 +253,24 @@ class CompuScope(HasMeasureTrigger, IsSensor, IsDaemon):
 
         return out
 
+    def close(self):
+        self._pg.free_system()
+
     def set_edge_width_count(self, count: int) -> None:
+        assert count >= 0  # no limits_getter atm
         self._state["edge_width_count"] = count
 
-    def set_record_count(self, count: int) -> int:
+    def set_record_count(self, count: int) -> None:
         self._state["record_count"] = count
-        return count
 
-    def set_segment_count(self, count: int) -> int:
+    def set_segment_count(self, count: int) -> None:
         self._state["segment_count"] = count
-        return count
+
+    def set_photon_threshold(self, threshold) -> None:
+        self._state["photon_threshold"] = threshold
+
+    def get_photon_threshold(self) -> float:
+        return self._state["photon_threshold"]
+
+    def get_photon_threshold_units(self) -> str:
+        return "V"
